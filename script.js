b5421b59e7782222b57243db77a9d9f778661f58
Starr Z. Chen <chenstarr@gmail.com>
2015-10-26 15:05:50 -0400

    Clicking right answer multiple times no longer continues to increase score

--- script.js
+++ script.js
@@ -6,13 +6,15 @@ $(document).ready(function(){
 });
 
 var clickCount = 0; //clicks per question should never exceed 1
-var questionCount = 1;
+var questionCount = 0;
 var score = 0;
 var setScore = function() {
-  $(".score").html("<p>" + score + " points</p>");
+  if (clickCount < 1) {
+    $(".score").html("<p>" + score + " points</p>");
+  }
 };
 
-var halloween = [
+var triviaPrompts = [
   // question : answer choices
   [ "Question 1", ["Answer 1A", "Answer 1B", "Answer 1C"] ],
   [ "Question 2", ["Answer 2A", "Answer 2B", "Answer 2C"] ],
@@ -21,21 +23,17 @@ var halloween = [
   [ "Question 5", ["Answer 5A", "Answer 5B", "Answer 5C"] ],
 ];
 
-// console.log(halloween[0]);
-// console.log(halloween[0][0]); //prints "Question 1"
-// console.log(halloween[0][1][2]); //prints "Answer 1C"
-
 // adds "Question 1" to the question div
 var addQuestion = function() {
-  $(".question").append(halloween[0][0]);
+  $(".question").append(triviaPrompts[questionCount][0]);
 };
 // addQuestion();
 
 // adds the answer choices to 3 answer divs
 var addAnswers = function(){
-  $("#A").text( halloween[0][1][0] );
-  $("#B").text( halloween[0][1][1] );
-  $("#C").text( halloween[0][1][2] );
+  $("#A").text( triviaPrompts[questionCount][1][0] );
+  $("#B").text( triviaPrompts[questionCount][1][1] );
+  $("#C").text( triviaPrompts[questionCount][1][2] );
 };
 // addAnswers();
 
@@ -46,33 +44,39 @@ var addAnswers = function(){
 
 var chooseAnswer = function(){
 
+  if (clickCount < 1) {
 
-  var rightAnswer = function() {
-    $(this).css("color", "white"); // (1)
-    $("#B").css("color", "grey"); $("#C").css("color", "grey"); //(2)
-    $(".right-or-wrong").show();
-    $(".right-or-wrong").text("Right!");
-    score = score + 10;
-    setScore();
-    questionCount++;
-  };
+    var rightAnswer = function() {
+      $(this).css("color", "white"); // (1)
+      $("#B").css("color", "grey"); $("#C").css("color", "grey"); //(2)
+      $(".right-or-wrong").show();
+      $(".right-or-wrong").text("Right!");
+      score = score + 10;
+      setScore();
+      clickCount++;
+      questionCount++;
+    };
 
-  $("#A").on("click", rightAnswer);
+    $("#A").on("click", rightAnswer);
 
-  // // clicking on a WRONG answer div: (1), (2), and (3) remain the same with different right-or-wrong text and different score
+    // // clicking on a WRONG answer div: (1), (2), and (3) remain the same with different right-or-wrong text and different score
 
-  var wrongAnswer = function () {
-    console.log(this);
+    var wrongAnswer = function () {
+      console.log(this);
 
-    $(".answer").css("color", "grey"); //(2)
-    $(this).css("color", "white"); // (1)
-    $(".right-or-wrong").show();
-    $(".right-or-wrong").text("Wrong, it was " + $("#A").text());
-    questionCount++;
-  };
+      $(".answer").css("color", "grey"); //(2)
+      $(this).css("color", "white"); // (1)
+      $(".right-or-wrong").show();
+      $(".right-or-wrong").text("Wrong, it was " + $("#A").text());
+      questionCount++;
+      clickCount++;
+    };
 
-  $("#B").on("click", wrongAnswer);
-  $("#C").on("click", wrongAnswer);
+    $("#B").on("click", wrongAnswer);
+    $("#C").on("click", wrongAnswer);
+  }
+  else {
+  }
 
 };
 
